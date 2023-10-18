abd0d437bb96f18b8d7e121be4721deb570283ef
Panda-Aadham <145044546+Panda-Aadham@users.noreply.github.com>
2023-10-18 16:01:58 +0100

    Homepage category grid

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,8 +2,26 @@ import React from "react";
 import "./Home.css";
 
 const Home = () => {
+    const categoryArray = [
+        ["Fruits", "Pasta", "Juice"],
+        ["Meats", "Fish", "Vegetables"],
+        ["Snacks", "Bakery", "Dairy"]
+    ];
+
     return (
         <div className="home-page">
+            <h1>Shoppy Shop</h1>
+            <div className="category-grid">
+                {categoryArray.map((row, rowIndex) => (
+                <div className="category-row" key={rowIndex}>
+                    {row.map((category, cellIndex) => (
+                    <button key={cellIndex} className="category-button">
+                        {category}
+                    </button>
+                    ))}
+                </div>
+                ))}
+            </div>
         </div>
     )
 }
