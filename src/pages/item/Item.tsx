3ca4a787d1a17846c0060b7bc8538055f3dd3ae0
Panda-Aadham <145044546+Panda-Aadham@users.noreply.github.com>
2023-10-20 11:29:54 +0100

    Added item picture + description

--- src/pages/item/Item.tsx
+++ src/pages/item/Item.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import categories from "../../data/categories.tsx";
-import { category, grocery } from "../../data/interfaces";
+import { grocery } from "../../data/interfaces";
 import { useParams } from "react-router-dom";
 import "./Item.css";
 
@@ -8,14 +8,16 @@ const Item = () => {
     const { categoryName, itemName } = useParams()
     const [itemData, setItemData] = useState<grocery>()
 
+    const showImage = true;
+
     useEffect(() => {
         categories.forEach((row) => {
             const foundCell = row.find(
                 (cell) => cell.title.toLowerCase() === categoryName)
             if (foundCell) {
-                setItemData(foundCell.items.find(
-                    (item) => item.title.toLowerCase() == itemName
-                    ))
+                setItemData(foundCell.items.find((item) =>
+                    item.title.toLowerCase().replace(" ","") == itemName
+                ))
             }
         })
     }, [])
@@ -26,6 +28,20 @@ const Item = () => {
             <header className="item-title">
                 <h1>{itemData.title}</h1>
             </header>
+            <div className="item-display">
+                {showImage && <img 
+                    src={itemData.image}
+                    className="item-display-image"/>}
+                <h3>{itemData.description}</h3>
+            </div>
+            <div className="item-control">
+                <h3>Quantity</h3>
+                <div className="item-cart-grid">
+                    <button className="item-cart-button">-</button>
+                    <span className="number">42</span>
+                    <button className="item-cart-button">+</button>
+                </div>
+            </div>
         </div>
     )
 }
