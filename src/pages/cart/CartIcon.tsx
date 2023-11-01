7b7d841407362a9aeda2f4da3d72678cbfff8d39
Panda-Aadham <145044546+Panda-Aadham@users.noreply.github.com>
2023-11-01 10:49:35 +0000

    Added cart item count

--- src/pages/cart/CartIcon.tsx
+++ src/pages/cart/CartIcon.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { useNavigate } from "react-router-dom";
+import "./CartIcon.css";
 
 const CartIcon = () => {
+    const [cartCount, setCartCount] = useState(0);
     const navigate = useNavigate();
 
     const handleClickCart = () => {
@@ -10,8 +12,17 @@ const CartIcon = () => {
         navigate("/cart")
     }
 
+    useEffect(() => {
+        const cartString = window.localStorage.getItem("cart")
+        if (cartString) {
+            const cart = JSON.parse(cartString)
+            setCartCount(cart.length)
+        }
+    }, [window.localStorage.getItem("cart")])
+
     return(
-        <div onClick={handleClickCart}>
+        <div className="cart-icon" onClick={handleClickCart}>
+            <div className="cart-circle">{cartCount}</div>
             <ShoppingCartIcon fontSize="large" className="home-cart"/>
         </div>
     )
